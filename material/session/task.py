bec4be6f5d207177ec9c66e77347262d238ec754
tokasherif108 <toka_sherif1008@hotmail.com>
2024-10-06 17:45:34 +0300

    task

---
+++ material/session/task.py
@@ -0,0 +1,16 @@
+print("hello world")
+x=10
+print(id(x))
+print(hex(id(x)))
+instructor='george'
+print(type(instructor))
+print("welcome to AMIT ML Course")
+y=20
+x=5+y
+print(type(x)) #complex#
+name="Toka"
+age="26"
+course_name="AI"
+print(().f("my name is"))
+
+
