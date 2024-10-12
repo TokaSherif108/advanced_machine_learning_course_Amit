7c1a2a240920f522e412720da8ee81319b51d6ab
tokasherif108 <toka_sherif1008@hotmail.com>
2024-10-12 10:24:32 +0300

    function python

--- session4functionspython.py/trials.py
+++ session4functionspython.py/trials.py
@@ -1,3 +1,31 @@
 def function1(x,y):
     print(f"first number ={x},second number={y}")
 function1(5,6)
+
+#we cant give default argument to the first one either both or the last one only 
+'''
+def function2(name="toka",age):
+'''
+'''def function2(name="TOKA",age=26):
+    return 
+def function3(name,age=26):
+    print()
+    '''
+'''
+y=input('write your name')
+name=y.split
+list=[name]
+def fprint(*x):
+    print(*x)
+fprint(list)
+'''
+list=[]
+numofname=int(input("please enter how many name do u have ? "))
+i=0
+while i<=numofname:
+    name=input("write name ")
+    list.append(name)
+    i+=1
+def fname(*x):
+    print(*x)
+fname(list)
