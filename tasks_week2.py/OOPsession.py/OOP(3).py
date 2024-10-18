3d158f1e36c523d046b495a070c8fcdb6a6577fc
tokasherif108 <toka_sherif1008@hotmail.com>
2024-10-18 11:04:54 +0300

    update

--- tasks_week2.py/OOPsession.py/OOP(3).py
+++ tasks_week2.py/OOPsession.py/OOP(3).py
@@ -2,6 +2,10 @@ class student():
     #method
     def __init__(self,name,age) -> None:
         pass
-        print("hello world")
+        self.name=name
+        self.age=age
+        #print("hello world")
+    def info(self):
+        print(f"name={self.name}")
 s1=student("toka",26)
-print(s1)
+print(s1.name)
