bd9806aa667bb8a68c8c79061ef6f33543e6743e
tokasherif108 <toka_sherif1008@hotmail.com>
2024-10-18 09:46:53 +0300

    update

--- tasks_week2.py/OOPsession.py/oopu.py
+++ tasks_week2.py/OOPsession.py/oopu.py
@@ -1,13 +1,14 @@
 class person:
     person_name="TOKA"
     person_age=26
-    def person_info(x):
+    def person_info(self):
         print("hello toka")
 p1=person()
 p2=person()
 p1.name="moh"
-print(p1.person_info())
-print(p1.__dict__)
+#print(p1.person_info())
+'''print(p1.__dict__)
 print(p2.__dict__)
-print(person.__dict__)
+print(person.__dict__)'''
 #print(p1.person_name)
+#print(person.person_info())
