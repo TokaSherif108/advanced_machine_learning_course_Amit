7e5b12b6ed7eeb44dc62eb5785f212980e06aed2
tokasherif108 <toka_sherif1008@hotmail.com>
2024-10-18 12:30:56 +0300

    update

---
+++ tasks_week2.py/OOPsession.py/calcwithclass.py
@@ -0,0 +1,10 @@
+class calculator:
+    def __init__(self,n1,n2) -> None:
+        pass
+        self.n1=n1
+        self.n2=n2
+    def sum(n1,n2):
+        return n1+n2
+    def subs(n1,n2):
+        return n1-n2
+    def 
