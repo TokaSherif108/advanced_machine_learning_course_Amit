e84883d86116bd95db7204c5a29c5be25449e141
tokasherif108 <toka_sherif1008@hotmail.com>
2024-11-01 10:16:18 +0200

    update

---
+++ NUMPY/nump.py
@@ -0,0 +1,6 @@
+#vector 
+import numpy as np
+m=np.array([[1,2,3],[4,"a",6],[7,8,9]])
+fmat=m.astype(float)
+print(fmat)
+print(fmat.dtype)
--- NUMPY/numpy.py
+++
@@ -1,8 +0,0 @@
-#vector 
-import numpy as np
-mat=np.array([[1,2,3],
-            [4,"a",6],
-            [7,8,9]])
-fmat=mat.astype(float)
-print(fmat)
-print(fmat.dtype)
