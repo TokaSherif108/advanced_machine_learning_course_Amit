c65778520c2ce4a0da2c796a8a59f1c9a17e1500
tokasherif108 <toka_sherif1008@hotmail.com>
2024-11-13 17:09:42 +0200

    update

--- problem_solving/century_from_year.py
+++ problem_solving/century_from_year.py
@@ -2,4 +2,7 @@ def century(year):
     # Finish this :)
         year=int(input(" enter year to know the century "))
         if year %100 == 0:
-            
+            print(year//100)
+        else:
+            print((year//100)+1)
+century(year=1998)
