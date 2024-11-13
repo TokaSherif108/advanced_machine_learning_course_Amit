9af2a435046dfd62bff30fe9015c7d1d7dedb629
tokasherif108 <toka_sherif1008@hotmail.com>
2024-11-13 16:06:05 +0200

    update

--- problem_solving/century_from_year.py
+++ problem_solving/century_from_year.py
@@ -1,9 +1,5 @@
 def century(year):
     # Finish this :)
-    year=int(input(" enter a year to check the century "))
-    if year[-1]and year[-2]== 0:
-        print(year[0:1])
-    else:
-        print(year[0:1]+1)
-        return 
-    century(year)
+        year=int(input(" enter year to know the century "))
+        if year %100 == 0:
+            
