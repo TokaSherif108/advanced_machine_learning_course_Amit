9f40b13657b604203c2929b68cd81fcbe573e641
tokasherif108 <toka_sherif1008@hotmail.com>
2024-11-16 09:44:26 +0200

    update

--- house_pricing_project/code.py
+++ house_pricing_project/code.py
@@ -11,7 +11,7 @@ from warnings import filterwarnings
 filterwarnings("ignore")
 pd.set_option('display.max_columns', None)
 df = pd.read_csv("Houses.csv")
-display(df.head(2))
+#display(df.head(2))
 print(df.shape)
 df.drop(["Id"], axis=1, inplace=True)
 pd.DataFrame({"Dtypes": df.dtypes, "Num_Uniq": df.nunique()}).T
